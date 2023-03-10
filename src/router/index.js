24fa5c0ab6b39d9eb150a4b65e1e64cac5b25d0a
dato.xvedelidze.888@gmail.com <116672666+xvedela@users.noreply.github.com>
2023-03-10 12:17:41 +0400

    2

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory, RouterView } from "vue-router";
+import Users from "../components/vueFiles/Profilelogo.vue";
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("../components/vuefiles/Profilelogo.vue"),
+      },
+      {
+        path: "/Log%20In",
+        name: "Register",
+        component: () => import("../components/vueFiles/Login.vue"),
+      },
+      {
+        path: "/Sign%20Up",
+        name: "Signup",
+        component: () => import("../components/vuefiles/Signup.vue"),
+      },
+    ]
+})
+
+export default router;
