b70e14b36c5d9a6128b9d30348a474fa83e60fbb
dato.xvedelidze.888@gmail.com <116672666+xvedela@users.noreply.github.com>
2023-03-10 17:13:32 +0400

    added .env

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import store from "./store"
 import router from './router'
 import './assets/main.css'
+import axios from 'axios'
 
     createApp(App)
     .use(store)
     .use(router)
     .mount('#app')
+
